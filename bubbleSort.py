bd29bea9573f9bfa6c003dd9253119171659a6da
Elizabeth Einstein <eeinstein134@gmail.com>
2020-03-03 12:22:09 -0700

    Added bubbleSort python, insertionSort

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+def bubbleSort(array):
+    sorted = False
+    
+    while not sorted:
+        sorted = True
+        for i in range(len(array) - 1):
+            if array[i] > array[i + 1]:
+                swap(i, i + 1, array)
+                sorted = False
+    
+    return array
+
+def swap(idx1, idx2, array):
+    array[idx1], array[idx2] = array[idx2], array[idx1]
