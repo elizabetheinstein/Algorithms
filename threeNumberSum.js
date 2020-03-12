401df0fd717524e2938397776f3253d08e19b3ed
Elizabeth Einstein <eeinstein134@gmail.com>
2020-03-12 10:36:09 -0600

    Added three number sum JS and Python

---
+++ threeNumberSum.js
@@ -0,0 +1,22 @@
+const threeNumberSum = (array, targetSum) => {
+    array.sort((a,b) => a - b)
+    let triplets = []
+    for (let i = 0; i < array.length - 2; i++) {
+        let current = array[i]
+        let left = i + 1
+        let right = array.length - 1
+        while (left < right) {
+            let currentSum = current + array[left] + array[right]
+            if (currentSum === targetSum) {
+                triplets.push([current, array[left], array[right]])
+                left++
+                right--
+            } else if (currentSum < targetSum) {
+                left++
+            } else if (currentSum > targetSum) {
+                right--
+            }
+        }
+    }
+    return triplets
+}
