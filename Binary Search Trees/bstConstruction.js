8a561123f7cb9b4a798f8b2e88b107f3948b7045
Elizabeth Einstein <eeinstein134@gmail.com>
2020-03-18 08:38:56 -0600

    Binary Search Tree: completed construction js

--- Binary Search Trees/bstConstruction.js
+++ Binary Search Trees/bstConstruction.js
@@ -6,44 +6,82 @@ class BST {
     }
 
     insert(value) {
-        if (value < this.value) {
-            if (this.left === null) {
-                this.left = new BST(value)
+        let currentNode = this
+        while (true) {
+            if (value < currentNode.value) {
+                if (currentNode.left === null) {
+                    currentNode.left = new BST(value)
+                    break
+                } else {
+                    currentNode = currentNode.left
+                }
             } else {
-                this.left.insert(value)
-            }
-        } else {
-            if (this.right === null) {
-                this.right = new BST(value)
-            } else {
-                this.right.insert(value)
+                if (currentNode.right === null) {
+                    currentNode.right = new BST(value)
+                    break
+                } else {
+                    currentNode = currentNode.right
+                }
             }
         }
         return this
     }
 
     contains(value) {
-        if (value === this.value) {
-            return true
-        } else {
-            if (value < this.value) {
-                if (this.left !== null) {
-                    return this.left.contains(value)
-                }
-            } else if (value > this.value) {
-                if (this.right !== null) {
-                    return this.right.contains(value)
-                }
+        let currentNode = this
+        while (currentNode !== null) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right
+            } else {
+                return true
             }
         }
         return false
     }
+    remove(value, parentNode = null) {
+        let currentNode = this
+        while (currentNode !== null) {
+            if (value < currentNode.value) {
+                parentNode = currentNode
+                currentNode = currentNode.left
+            } else if (value > currentNode.value) {
+                parentNode = currentNode
+                currentNode = currentNode.right
+            } else {
+                if (currentNode.left !== null && currentNode.right !== null) {
+                    currentNode.value = currentNode.right.getMinVal()
+                    currentNode.right.remove(currentNode.value, currentNode)
+                } else if (parentNode === null) {
+                    if (currentNode.left !== null) {
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    } else if (currentNode.right !== null) {
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    } else {
+                        currentNode.value = null
+                    }
+                } else if (parentNode.left === currentNode) {
+                    parentNode.left = currentNode.left !== null ? currentNode.left : currentNode.right
+                } else if (parentNode.right === currentNode) {
+                    parentNode.right = currentNode.left !== null ? currentNode.left : currentNode.right
+                }
+                break
+            }
+        }
+        return this
+    }
 
     getMinVal() {
-        if (this.left !== null) {
-            return this.left.getMinVal()
+        let currentNode = this
+        while (currentNode.left !== null) {
+            currentNode = currentNode.left
         }
-        return this.value
+        return currentNode.value
     }
 }
 
