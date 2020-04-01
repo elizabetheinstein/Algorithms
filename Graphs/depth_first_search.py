82a6ea6f4ff4d999867c2abef14ddf926d872527
Elizabeth Einstein <eeinstein134@gmail.com>
2020-04-01 09:42:22 -0600

    Graphs: depth first search updated function name syntax python

--- Graphs/depth_first_search.py
+++ Graphs/depth_first_search.py
@@ -3,11 +3,11 @@ class Node:
         self.children = []
         self.name = name
 
-    def addChild(self, name):
+    def add_child(self, name):
         self.children.append(Node(name))
         return self
 
-    def depthFirstSearch(self, array):
+    def depth_first_search(self, array):
         array.append(self.name)
         for child in self.children:
             child.depthFirstSearch(array)
