539e4f8f22d073791178a43ec5e32ab139623088
Elizabeth Einstein <eeinstein134@gmail.com>
2020-04-07 19:53:31 -0600

    Recursion: added iterative and recursive solution js

--- Recursion/powerSet.js
+++ Recursion/powerSet.js
@@ -1,4 +1,21 @@
-const powerSet = (array) => {
+//recursive solution
+const powerSet = (array, idx = null) => {
+    if (idx === null) idx = array.length - 1
+    if (idx < 0) return [[]]
+
+    const element = array[idx]
+    const subsets = powerSet(array, idx - 1)
+    const length = subsets.length
+    for (let i = 0; i < length; i++) {
+        const current = subsets[i]
+        subsets.push(current.concat(element))
+    }
+    return subsets
+}
+
+//iterative solution
+
+const powerSet1 = (array) => {
     let powerArr = [[]]
 	for (const element of array) {
 		let powerLen = powerArr.length
