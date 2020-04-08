c5230ef0af5ce0191683ac56aa13f75c06d2aa8d
Elizabeth Einstein <eeinstein134@gmail.com>
2020-04-08 17:36:56 -0600

    Recursion: get nth fibonacci python

---
+++ Recursion/get_nth_fib.py
@@ -0,0 +1,18 @@
+#recursive solution
+def get_nth_fib(n, memoize = {1: 0, 2: 1}):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = get_nth_fib(n - 1) + get_nth_fib(n - 2)
+        return memoize[n]
+
+# iterative solution
+def get_nth_fib_1(n):
+    last_two = [0, 1]
+    counter = 3
+    while counter <= n:
+        next_fib = last_two[0] + last_two[1]
+        last_two[0] = last_two[1]
+        last_two[1] = next_fib
+        counter += 1
+    return last_two[1] if n > 1 else last_two[0]
