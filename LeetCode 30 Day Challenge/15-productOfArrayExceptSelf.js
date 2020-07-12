2513915e31aadf0b8d3a6bedf3a7b8a7c70dd073
Elizabeth Einstein <eeinstein134@gmail.com>
2020-07-12 14:33:23 -0600

    Leetcode Day 15 product of array except self js

---
+++ LeetCode 30 Day Challenge/15-productOfArrayExceptSelf.js
@@ -0,0 +1,15 @@
+const productExceptSelf = (nums) => {
+    const output = new Array(nums.length).fill(1)
+    let left = 1
+    for (let i = 0; i < nums.length; i++) {
+        output[i] *= left
+        left *= nums[i]
+    }
+    let right = 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        output[i] *= right
+        right *= nums[i]
+    }
+
+    return output
+}
